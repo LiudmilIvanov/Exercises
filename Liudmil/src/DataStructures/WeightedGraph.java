383306a8acaddf0ec3eb9540e6e0feae33a76d7c
ACER <ACER@PC>
2019-11-01 08:22:05 +0200

    Weighted graph representation

---
+++ Liudmil/src/DataStructures/WeightedGraph.java
@@ -0,0 +1,52 @@
+package DataStructures;
+
+import java.util.LinkedList;
+
+public class WeightedGraph {
+
+	static class Edge {
+		int source;
+		int destination;
+		int weight;
+
+		public Edge(int source, int destination, int weight) {
+			this.source = source;
+			this.destination = destination;
+			this.weight = weight;
+		}
+	}
+
+	int vertices;
+	LinkedList<Edge> adjList[];
+
+	public WeightedGraph(int vertices) {
+		this.vertices = vertices;
+		adjList = new LinkedList[vertices];
+		for (int i = 0; i < vertices; i++) {
+			adjList[i] = new LinkedList<Edge>();
+		}
+	}
+
+	public void addEdge(int source, int destination, int weight) {
+		Edge edge = new Edge(source, destination, weight);
+		adjList[source].addFirst(edge);
+	}
+
+	public void printGraph() {
+		for (int i = 0; i < vertices; i++) {
+			LinkedList<Edge> list = adjList[i];
+			for (int j = 0; j < list.size(); j++) {
+				System.out.println("Vertex " + i + "is connected to " + list.get(j).destination + " with weight"
+						+ list.get(j).weight);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		WeightedGraph graph = new WeightedGraph(2);
+		graph.addEdge(0, 1, 2);
+		graph.addEdge(1, 2, 3);
+		graph.printGraph();
+	}
+
+}
